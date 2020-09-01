6fdd3bbcfb6dd19e1c0f6808c0528be71f1e0613
Diplomat14 <diplomt@gmail.com>
2020-09-01 18:40:59 +0900

    -

---
+++ README.rst
@@ -0,0 +1,4 @@
+JiraAutomation Installation
+--------
+
+This is a root JiraAutomation project repository. In has installation, configuration and running scripts
---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+import os
+
+def readme():
+    with open(os.path.dirname(os.path.abspath(__file__)) + '/README.rst') as f:
+        return f.read()
+
+setup(
+    name='jiraautomationinstallation',
+    version='0.1',
+    description=readme(),
+    long_description=readme(),
+    url='',
+    author='Aleksey Denysyuk',
+    author_email='diplomt@gmail.com',
+    license='MIT', #TBD
+    packages=[],
+    install_requires=[
+        'jacustomization @ git+ssh://git@github.com/Diplomat14/jiraautomationcustomization.git'
+    ],
+    include_package_data=True,
+    test_suite='nose.collector',
+    tests_require=['nose'],
+    zip_safe=False
+)
